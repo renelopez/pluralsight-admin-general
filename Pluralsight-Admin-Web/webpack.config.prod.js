a74c46ec45745021121d10d592c1b5b0c6d5fa29
rene.lopez <rene.lopez.cano@gmail.com>
2019-04-16 11:12:42 -0500

    Added mocha reporter

--- Pluralsight-Admin-Web/webpack.config.prod.js
+++ Pluralsight-Admin-Web/webpack.config.prod.js
@@ -26,7 +26,7 @@ module.exports = {
 
     new webpack.DefinePlugin({
       "process.env.API_URL": JSON.stringify("http://localhost:3001"),
-      "process.env.TEAMCITY_VERSION":process.env.TEAMCITY_VERSION,
+      "process.env.TEAMCITY_VERSION": process.env.TEAMCITY_VERSION,
       // This global makes sure React is built in production mode.
       "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
     }),
